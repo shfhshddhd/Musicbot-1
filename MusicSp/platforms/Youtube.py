d732527e411eb99b6a941ce97c07de889940ec25
shfhshddhd <onebhaiabcd@gmail.com>
2026-09-26 06:33:21 +0530

    Remove unused YouTube import

--- MusicSp/platforms/Youtube.py
+++ MusicSp/platforms/Youtube.py
@@ -3,7 +3,6 @@ import os
 import re
 from typing import Union
 import yt_dlp
-import json
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from py_yt import VideosSearch, Playlist
