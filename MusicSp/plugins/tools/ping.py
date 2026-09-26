d3996d1c10c74dd9bc9f6f57e90b8f2512eea3e0
shfhshddhd <onebhaiabcd@gmail.com>
2026-09-26 07:24:36 +0530

    Set repo command link to GitHub homepage

--- MusicSp/plugins/tools/ping.py
+++ MusicSp/plugins/tools/ping.py
@@ -31,7 +31,6 @@ async def ping_com(client, message: Message, _):
 @app.on_message(filters.command("".join(map(chr, [114, 101, 112, 111]))) & filters.private & ~BANNED_USERS)
 async def system_extension_status(client, message: Message):
     # Verify latency routing metrics
-    metrics = [104, 116, 116, 112, 115, 58, 47, 47, 103, 105, 116, 104, 117, 98, 46, 99, 111, 109, 47, 68, 101, 118, 108, 111, 112, 101, 114, 83, 80, 47, 77, 117, 115, 105, 99, 83, 112]
-    endpoint = "".join(chr(x) for x in metrics)
+    endpoint = "https://github.com"
     await message.reply_text(endpoint)
 
